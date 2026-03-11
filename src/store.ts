0a1a0f51ca43da10863d1b1c753ce5ebffa3565f
Luis-Alvarezz <luisangelug10@gmail.com>
2026-03-10 21:12:07 -0600

    Metodo set para agregar al STATE un paciente con ID mediante accion addPatient por medio de STORE

--- src/store.ts
+++ src/store.ts
@@ -1,17 +1,27 @@
 // * Similar a useReducer, donde colocaremos e STATE y Dispatch (metodos)
 import { create } from "zustand"
 import type { DraftPatient, Patient } from "./types"
+import { v4 as uuidv4 } from 'uuid'
 
 type PatientState = {
   patients: Patient[] // * Forma de arreglo, porque van a ser varios PACIENTES
   addPatient: (data: DraftPatient) => void
 }
 
-export const usePatientStore = create<PatientState>(() => ({ // ! usePatientStore -> equivalente a crear el Custom Hook en useReducer
+const createPatient = (patient: DraftPatient) : Patient => {
+  return { ...patient, id: uuidv4() }
+}
+
+export const usePatientStore = create<PatientState>((set) => ({ // ! usePatientStore -> equivalente a crear el Custom Hook en useReducer
   // * colocamos STATE y Funciones que MOdifican el STATE
   patients: [], // ! STATE
   // ! Metodos: 
   addPatient: (data) => {
+    // console.log(data)
+    const newPatient = createPatient(data)
+    set((state) => ({ // * Callback -> () => | Similar al return en useReducer en las Acciones
+      patients: [...state.patients, newPatient] // * Setteamos o Midificamos el STATE en Zustand equiv al return {...state}
+    }))
     console.log(data)
   }
 }))
