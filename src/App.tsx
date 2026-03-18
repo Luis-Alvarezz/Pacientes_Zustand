f8533e6f62fdf71ba929dfcbac9ff2c137cacd95
Luis-Alvarezz <luisangelug10@gmail.com>
2026-03-17 23:56:41 -0600

    [ADD]: Componente de Toast para renderizar alartas en App principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { PatientForm } from "./components/PatientForm"
 import { PatientsList } from "./components/PatientsList"
+import { ToastContainer } from "react-toastify"
 
 function App() {
 
@@ -16,6 +17,8 @@ function App() {
         <PatientsList />
       </div>
     </div>
+
+    <ToastContainer />
     </>
   )
 }
